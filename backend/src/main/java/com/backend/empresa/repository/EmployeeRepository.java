a617cb674ed6351405d5a141a28ad41b9c9fbe1b
Raul <raul.renner.9824@outlook.com>
2022-02-18 04:00:28 -0300

    subindo aplicacao com front e backend da aplicacao

---
+++ backend/src/main/java/com/backend/empresa/repository/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.backend.empresa.repository;
+
+import com.backend.empresa.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+}
